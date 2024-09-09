d484634c6b47690a093c7e6d631d4ffe0e5aafd7
jereli0410 <52683400+jereli0410@users.noreply.github.com>
2024-09-09 21:07:23 +0800

    Fix again

--- lib/Water/Water.cpp
+++ lib/Water/Water.cpp
@@ -22,7 +22,7 @@ Water::~Water(){}
 
 bool Water::isPotable(){
     bool ispHLevelOK = _pHLevel > _pHThreshold-1.5&&_pHLevel < _pHThreshold+1.5;
-    bool isTotalDissolvedSolidsOK = _totaldissolvedsolids > _totaldissolvedsolidsThreshold+175&&_totaldissolvedsolids < _totaldissolvedsolidsThreshold-175;
+    bool isTotalDissolvedSolidsOK = _totaldissolvedsolids > _totaldissolvedsolidsThreshold-175&&_totaldissolvedsolids < _totaldissolvedsolidsThreshold+175;
 
     return ispHLevelOK && isTotalDissolvedSolidsOK;
 }
